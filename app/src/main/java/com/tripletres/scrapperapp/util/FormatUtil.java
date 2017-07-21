6929fbbdcb7676dd9843c8421da6bb5fa2fcd203
daniel <pedro@ievolutioned.com>
2017-07-20 20:37:14 -0500

    [Added] format for url

--- app/src/main/java/com/tripletres/scrapperapp/util/FormatUtil.java
+++ app/src/main/java/com/tripletres/scrapperapp/util/FormatUtil.java
@@ -1,5 +1,8 @@
 package com.tripletres.scrapperapp.util;
 
+import android.text.TextUtils;
+import android.webkit.URLUtil;
+
 /**
  * Format utility class.
  * <p>
@@ -7,8 +10,23 @@ package com.tripletres.scrapperapp.util;
  */
 
 public class FormatUtil {
-    public static String getUrl(String msg) {
-        //TODO: check if it an valid url
-        return msg;
+
+    /**
+     * Gets an URL from a string
+     *
+     * @param s - Target string
+     * @return an url string, null otherwise
+     */
+    public static String getUrl(String s) {
+        //Should I contradict Google? https://developer.android.com/reference/android/webkit/URLUtil.html
+        if (s != null && !TextUtils.isEmpty(s)) {
+            String[] split = s.split("\\s+");
+            for (String part : split) {
+                if (URLUtil.isHttpUrl(part) || URLUtil.isHttpsUrl(part)) {
+                    return part;
+                }
+            }
+        }
+        return null;
     }
 }
